980cc0954ef341cccc0b84b8a6abb4793eb99d39
Serge Aleynikov <saleyn@gmail.com>
2022-02-08 19:30:48 -0500

    Update pacaur install

--- util/install-pacaur.sh
+++ util/install-pacaur.sh
@@ -12,18 +12,20 @@ export PATH=$PATH:/usr/bin/core_perl
 
 mkdir -p /tmp/build
 cd $_
-mkdir -p cower
-pushd $_
 #wget -O PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-wget -O PKGBUILD https://raw.githubusercontent.com/saleyn/archlinux/master/util/cower.PKGBUILD
+#wget -O PKGBUILD https://raw.githubusercontent.com/saleyn/archlinux/master/util/cower.PKGBUILD
+wget -O PKGBUILD https://aur.archlinux.org/cgit/aur.git/snapshot/auracle-git.tar.gz
+tar -xzf auracle-git.tar.gz
+pushd auracle-git
 makepkg -i -s --noconfirm
 popd
-mkdir -p expac
-pushd $_
+#mkdir -p expac
+#pushd $_
 #wget -O PKGBUILD https://projects.archlinux.org/svntogit/packages.git/plain/trunk/PKGBUILD?h=packages/expac
-wget -O PKGBUILD https://raw.githubusercontent.com/saleyn/archlinux/master/util/expac.PKGBUILD
-makepkg -i --noconfirm
-popd
+#wget -O PKGBUILD https://raw.githubusercontent.com/saleyn/archlinux/master/util/expac.PKGBUILD
+#makepkg -i --noconfirm
+#popd
+sudo pacman -S meson fakechroot gtest patch
 mkdir -p pacaur
 pushd $_
 wget -O PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
